9afadb9f165e947337e017656e2f680a8422e2e4
justint3ng <justint3ng@gmail.com>
2025-10-14 13:05:52 +1000

    vectorquantizer
    created a rough template for a vector quantizer.

--- recognition/GANmodel_47508042/modules.py
+++ recognition/GANmodel_47508042/modules.py
@@ -30,3 +30,47 @@ class Decoder(nn.Module):
 
     def forward(self, z):
         return self.dec(z)
+
+class VectorQuantizer(nn.Module):
+    def __init__(self, num_embeddings=512, embedding_dim=64, beta=0.25):
+        super().__init__()
+        self.num_embeddings = num_embeddings
+        self.embedding_dim = embedding_dim
+        self.beta = beta
+
+        self.embedding = nn.Embedding(self.num_embeddings, self.embedding_dim)
+        nn.init.uniform_(self.embedding.weight, -1.0 / self.num_embeddings, 1.0 / self.num_embeddings)
+
+    def forward(self, z):
+        z_perm = z.permute(0, 2, 3, 1).contagious()
+        flat_z = z_perm.view(-1, self.embedding_dim)
+
+        # compute distances
+        distances = (
+            torch.sum(flat_z**2, dim=1, keepdim=True)
+            + torch.sum(self.embedding.weight**2, dim=1)
+            - 2 * torch.matmul(flat_z, self.embedding.weight.t())
+        )
+
+        encoding_indices = torch.argmin(distances, dim=1).unsqueeze(1)
+        encodings = torch.zeros(encoding_indices.size(0), self.num_embeddings, device=z.device)
+        encodings.scatter_(1, encoding_indices, 1)
+
+        # quantized
+        quantized = torch.matmul(encodings, self.embeddings.weights)
+        quantized = quantized.view(z_perm.shape)
+        quantized = quantized.permute(0, 3, 1, 2).contiguous()
+
+        # losses
+        e_latent_loss = F.mse_loss(quantized.detach(), z)
+        q_latent_loss = F.mse_loss(quantized, z.detach())
+        loss = q_latent_loss + self.beta * e_latent_loss
+
+        # estimator
+        quantized = z + (quantized - z).detach()
+
+        # visualisation
+        encoding_indices = encoding_indices.view(z.shape[0], z.shape[2], z.shape[3])
+        return quantized, loss, encoding_indices
+
+
